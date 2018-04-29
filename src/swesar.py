0bbe29ada6b8453d3b9897d540f76af20e1bf349
Luke Webber <lukewebber@gmail.com>
2018-04-29 16:05:17 +0200

    Added main method and concurrent downloads. Added help text and description.

--- src/swesar.py
+++ src/swesar.py
@@ -1,7 +1,9 @@
 import requests
 import shutil
 import xmltodict
-import sys
+import asyncio
+import argparse
+import concurrent.futures
 
 
 def load_products(fp):
@@ -13,7 +15,11 @@ def load_products(fp):
     """
     with open(fp) as fh:
         xml_doc = xmltodict.parse(fh.read())
-    return [obj['@name'] for obj in xml_doc['metalink']['file']]
+    try:
+        return [obj['@name'] for obj in xml_doc['metalink']['file']]
+    except TypeError:
+        # If products.meta4 has only one file, the returned xmltodict is not iterable
+        return [xml_doc['metalink']['file']['@name']]
 
 
 def gen_swea_name(esa_name):
@@ -24,7 +30,7 @@ def gen_swea_name(esa_name):
     :return: (String) Swea formatted product name
     """
     p = list(filter(None, esa_name.split('_')))
-    return "{mission}{time}_{type}SAR{mode}_%".format(mission=p[0], time=p[4][2:].replace('T', ''), type=p[2],
+    return "{mission}{time}_{type}SAR{mode}_%".format(mission=p[0], time=p[4][2:].replace('T', ''), type=p[2][:3],
                                                       mode=p[1])
 
 
@@ -63,9 +69,26 @@ def get_swea_prod(swea_id):
         shutil.copyfileobj(request_obj.raw, fh)
 
 
-def main(argv):
-    pass
+async def main(args):
+    if args.products.endswith(".meta4"):
+        esa_products = load_products(args.products)
+    else:
+        esa_products = [args.products]
+
+    swea_products = [gen_swea_name(esa_name) for esa_name in esa_products]
+    swea_product_ids = [gen_swea_id(swea_name) for swea_name in swea_products]
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        loop = asyncio.get_event_loop()
+        futures = [loop.run_in_executor(executor, get_swea_prod, swea_id) for swea_id in swea_product_ids]
+        await asyncio.gather(*futures)
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    parser = argparse.ArgumentParser(description="Terminal client for downloading images for Swea Portal.")
+    parser.add_argument('products',
+                        help="ESA Product name, or filepath to downloaded cart. e.g. S1A_IW_GRDH_1SDV_20180224T162111_20180224T162136_020751_0238FE_48D1 or products.meta4")
+    args = parser.parse_args()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(args))
